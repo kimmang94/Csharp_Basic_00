2561154be18c5d3a98b761ee58b7e3c3e06aa337
kimhyeok <kimmang940715@naver.com>
2023-10-02 17:59:16 +0900

    Hello World Args

---
+++ _001_HelloWorld/_001_HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello World : " + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Hello World : agrs.Length = 0");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
