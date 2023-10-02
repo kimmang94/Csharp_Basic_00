990215447373ce7bfb69375ae4f3a86436f7f108
kimhyeok <kimmang940715@naver.com>
2023-10-02 17:13:31 +0900

    Hello World 알아보기 추가

---
+++ _001_HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * using : 선언된 네임스페이스 내부 개체들에 접근
+ * namespace : 그룹화, 동일한 함수의 이름 사용 가능
+ * class : Field, Method 로 분리, 프로그램을 구성하는 기본
+ */
+
+namespace _001_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
