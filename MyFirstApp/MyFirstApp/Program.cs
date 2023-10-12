836f04dbcda7828e4f5314ff95c25c449c755489
kimhyeok <kimmang940715@naver.com>
2023-10-12 18:34:25 +0900

    기본 구성에대한 이해 및 emd로 실행

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) 
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            Console.WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
--- MyFirstApp/MyFirstApp/Properties/AssemblyInfo.cs
+++ MyFirstApp/MyFirstApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("_004_TypeConversion")]
+[assembly: AssemblyTitle("MyFirstApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("_004_TypeConversion")]
+[assembly: AssemblyProduct("MyFirstApp")]
 [assembly: AssemblyCopyright("Copyright © HP 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("e550c637-bdce-4915-90b8-0ed01c0ce5d2")]
+[assembly: Guid("083a95ca-068b-4fb0-9021-3235e70539a7")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
