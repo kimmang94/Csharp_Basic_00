1d1f961cf38d38812ee2c2247075510d4ecf95e9
kimhyeok <kimmang940715@naver.com>
2024-04-21 02:48:28 +0900

    Hello world 분석

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    /* using 키워드 : 선언된 네임스페이스 내부 개체들에 접근
+     * namespace 키워드 : 그룹화, 동일한 함수 이름 사용가능
+     * class 키워드 : 필드(field)와 메소드(Method)로 분리, 프로그램을 구성하는 기본
+     * 세미콜론 ; : 코드 문장의 끝
+     * 브레이스 { } : 코드 블럭 (블럭단위로 코드 구분)
+     * static void Main : 가장 중요한 메소드 , 최초으 ㅣ진입점, 메인 메소드가 반드시존재 
+     * 형태가 다양한 Main 함수 : 함수가 가지는 특성
+     */
+}
