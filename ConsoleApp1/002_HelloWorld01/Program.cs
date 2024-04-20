41a03efd7f908496083108e0064a2ba729673280
kimhyeok <kimmang940715@naver.com>
2024-04-21 02:56:09 +0900

    args 다루기

---
+++ ConsoleApp1/002_HelloWorld01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _002_HelloWorld01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello World  : " + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Hello World : args.Length = 0" );
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
