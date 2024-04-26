8f723e0f3a09850573ff9b4f006602b03e7ca273
kimhyeok <kimmang940715@naver.com>
2024-04-27 03:33:00 +0900

    feat : Data 다루기 1

---
+++ ConsoleApp1/003_Variable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = 100;
+
+            Console.WriteLine($"num : {num}");
+        }
+    }
+}
